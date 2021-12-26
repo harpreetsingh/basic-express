3f2d4e35214f169cf77f00fe53c301907f4da73d
Harpreet Singh <hsingh@launchableinc.com>
2021-12-26 14:41:46 -0800

    Added bootstrap

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 A HelloWorld website using express
 
+from [Udemy Course](https://www.udemy.com/course/learn-nodejs-by-building-10-projects/)
 Uses Node, Jade template engine, bootstrap
 
-from [Udemy Course](https://www.udemy.com/course/learn-nodejs-by-building-10-projects/)
+
+Used Html2jade converter to convert bootstrap to jade code
--- app.js
+++ app.js
@@ -10,11 +10,21 @@ var app = express ();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// using stylesheets
+app.use(express.static(path.join(__dirname,'public')));
+
+
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', {title: 'BlueOrange'});
 });
+
+app.get('/about', function(req, res){
+    res.render('about', {title: 'About BlueOrange'});
+});
+
 app.listen(3001);
 console.log('Server is running on port 3001');
